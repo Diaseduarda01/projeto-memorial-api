10d5744c3abe70892381ed9824a00ce0b5c2a2fd
Eduarda Dias <maria.dias@sptech.school>
2025-11-19 15:35:01 -0300

    feat(app): metodos de busca personalizados no repository

--- src/main/java/memorial/luiz/api/repository/RoleRepository.java
+++ src/main/java/memorial/luiz/api/repository/RoleRepository.java
@@ -4,4 +4,5 @@ import memorial.luiz.api.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Role findByNomeIgnoreCase(String nome);
 }
--- src/main/java/memorial/luiz/api/repository/StatusRepository.java
+++ src/main/java/memorial/luiz/api/repository/StatusRepository.java
@@ -4,4 +4,5 @@ import memorial.luiz.api.entity.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StatusRepository extends JpaRepository<Status, Integer> {
+    Status findByNomeIgnoreCase(String nome);
 }

67001278d923b7ff752e5254b48db42334c353bb
Eduarda Dias <maria.dias@sptech.school>
2026-01-14 10:06:59 -0300

    feat(app): config de cors

--- src/main/java/memorial/luiz/api/config/WebConfig.java
+++ src/main/java/memorial/luiz/api/config/WebConfig.java
@@ -1,11 +1,21 @@
 package memorial.luiz.api.config;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Component
 public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(false);
+    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {

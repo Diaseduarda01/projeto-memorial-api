c98f89557b40e939ef33d45b5d0ea3405c5568ce
Eduarda Dias <maria.dias@sptech.school>
2026-01-14 13:26:38 -0300

    feat(app): add url ao salvar

--- src/main/java/memorial/luiz/api/dto/midia/MidiaResponseDto.java
+++ src/main/java/memorial/luiz/api/dto/midia/MidiaResponseDto.java
@@ -1,6 +1,5 @@
 package memorial.luiz.api.dto.midia;
 
-import memorial.luiz.api.entity.Midia;
 import memorial.luiz.api.entity.TipoMidia;
 
 public record MidiaResponseDto(
@@ -9,12 +8,4 @@ public record MidiaResponseDto(
         String url,
         TipoMidia tipo
 ) {
-    public static MidiaResponseDto from(Midia midia) {
-        return new MidiaResponseDto(
-                midia.getId(),
-                midia.getNome(),
-                "https://projeto-memorial-api.onrender.com/upload/" + midia.getCaminho(),
-                midia.getTipo()
-        );
-    }
 }
